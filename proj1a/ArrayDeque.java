4bb9782695e34caacd356a9ba314053fdc8b7de2
Huapeng Zhou <zhouhp.me@gmail.com>
2022-08-25 15:10:50 +0800

    2022/8/25 copy flyingPig

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,6 +4,7 @@ public class ArrayDeque <T>{
     private int nextFirst;
     private int nextLast;
 
+    private static int refector = 2;
     public ArrayDeque(){
         size = 0;
         nextFirst = 4;
@@ -19,13 +20,37 @@ public class ArrayDeque <T>{
     }
 
     public void removeLast(){
-
+        size -= 1;
+        nextLast -= 1;
+        if(size == item.length / (2 * refector)){
+            resize(size / refector);
+        }
     }
 
+    public void removeFirst() {
+        size -= 1;
+        nextFirst += 1;
+        if(size == item.length / (2 * refector)){
+            resize(size / refector, nextFirst + 1, nextLast - 1);
+        }
+    }
+    private void resize(int newSize, int startIndex, int endIndex){
+        T[] newItem = (T[]) new Object[newSize];
+        if (startIndex <= endIndex){
+
+        }
+        System.arraycopy(item, 0, newItem, 0, size);
+        size = newSize;
+        item = newItem;
+    }
     public void addFirst(T addedFirstItem){
         item[nextFirst] = addedFirstItem;
         nextFirst -= 1;
-        if (nextFirst < 0){
+        size += 1;
+        if (size == item.length){
+            resize(size * refector, 0, 0);
+        }
+        if (nextFirst == -1){
             nextFirst = item.length - 1;
         }
     }
@@ -33,6 +58,13 @@ public class ArrayDeque <T>{
     public void addLast(T addedLastItem){
         item[nextLast] = addedLastItem;
         nextLast += 1;
+        size += 1;
+        if (size == item.length){
+            resize(size * refector);
+        }
+        if (nextLast == item.length){
+            nextLast = 0;
+        }
     }
 
     public static void main(String[] args) {
